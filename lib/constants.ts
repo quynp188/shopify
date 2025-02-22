cea13a4bba66721d7ffb81e159a18311ab81388e
quynp188 <nguyenphuquyfuv@gmail.com>
2025-02-23 02:01:39 +0700

    build layout page

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'SHOPIFY'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Buy anything, enjoy more.'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'An Shopify clone built with Next.js and MongoDB'
