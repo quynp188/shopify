85217639b518cda84fdfda495505e44c76986d24
quynp188 <nguyenphuquyfuv@gmail.com>
2025-02-23 15:42:12 +0700

    create best seling price

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,7 @@
 import { HomeCard } from '@/components/shared/home/home-card'
 import { HomeCarousel } from '@/components/shared/home/home-carousel'
 import ProductSlider from '@/components/shared/product/product-slider'
-import { CardContent } from '@/components/ui/card'
+import { Card, CardContent } from '@/components/ui/card'
 import {
   getAllCategories,
   getProductsByTag,
@@ -63,14 +63,27 @@ export default async function HomePage() {
     },
   ]
   const todaysDeals = await getProductsByTag({ tag: 'todays-deal' })
+  const bestSellingProducts = await getProductsByTag({ tag: 'best-seller' })
+
   return (
     <>
       <HomeCarousel items={data.carousels} />
       <div className="md:p-4 md:space-y-4 bg-border">
         <HomeCard cards={cards} />
-        <CardContent className="p-4 items-center gap-3">
-          <ProductSlider title={"Today's Deal"} products={todaysDeals} />
-        </CardContent>
+        <Card className="w-full rounded-none">
+          <CardContent className="p-4 items-center gap-3">
+            <ProductSlider title={"Today's Deal"} products={todaysDeals} />
+          </CardContent>
+        </Card>
+        <Card className="w-full rounded-none">
+          <CardContent className="p-4 items-center gap-3">
+            <ProductSlider
+              title="Best Selling Products"
+              products={bestSellingProducts}
+              hideDetails
+            />
+          </CardContent>
+        </Card>
       </div>
     </>
   )
