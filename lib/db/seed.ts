5dcaa7024b0880643c31f8ef8aa698722a405331
quynp188 <nguyenphuquyfuv@gmail.com>
2025-02-25 20:25:15 +0700

    sign in

--- lib/db/seed.ts
+++ lib/db/seed.ts
@@ -3,19 +3,24 @@ import { connectToDatabase } from '.'
 import Product from './models/product.model'
 import { cwd } from 'process'
 import { loadEnvConfig } from '@next/env'
+import User from './models/user.model'
 
 loadEnvConfig(cwd())
 
 const main = async () => {
   try {
-    const { products } = data
+    const { products, users } = data
     await connectToDatabase(process.env.MONGODB_URI)
 
     await Product.deleteMany()
+    await User.deleteMany()
+
+    const createdUsers = await User.insertMany(users)
     const createdProducts = await Product.insertMany(products)
 
     console.log({
       createdProducts,
+      createdUsers,
       message: 'Seeded database successfully',
     })
     process.exit(0)
