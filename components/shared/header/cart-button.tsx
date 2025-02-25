3051d4b237bb4022e7903261c50df3d7d7f4d4b0
quynp188 <nguyenphuquyfuv@gmail.com>
2025-02-25 10:29:28 +0700

    cart slide bar

--- components/shared/header/cart-button.tsx
+++ components/shared/header/cart-button.tsx
@@ -5,18 +5,19 @@ import Link from 'next/link'
 import useIsMounted from '@/hooks/use-is-mounted'
 import { cn } from '@/lib/utils'
 import useCartStore from '@/hooks/use-cart-store'
+import useCartSidebar from '@/hooks/use-cart-sidebar'
 
 export default function CartButton() {
   const isMounted = useIsMounted()
   const {
     cart: { items },
   } = useCartStore()
+  const isCartSidebarOpen = useCartSidebar()
   const cartItemsCount = items.reduce((a, c) => a + c.quantity, 0)
   return (
     <Link href="/cart" className="px-1 header-button">
       <div className="flex items-end text-xs relative">
         <ShoppingCartIcon className="h-8 w-8" />
-
         {isMounted && (
           <span
             className={cn(
@@ -28,6 +29,11 @@ export default function CartButton() {
           </span>
         )}
         <span className="font-bold">Cart</span>
+        {isCartSidebarOpen && (
+          <div
+            className={`absolute top-[20px] right-[-16px] rotate-[-90deg] z-10 w-0 h-0 border-l-[7px] border-r-[7px] border-b-[8px] border-transparent border-b-background`}
+          ></div>
+        )}
       </div>
     </Link>
   )
