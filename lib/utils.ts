d6461257eed0d007460177af6e5ddb67873a8703
quynp188 <nguyenphuquyfuv@gmail.com>
2025-03-19 06:55:03 +0700

    CREATE ORDER

--- lib/utils.ts
+++ lib/utils.ts
@@ -133,3 +133,26 @@ export const formatDateTime = (dateString: Date) => {
 export function formatId(id: string) {
   return `..${id.substring(id.length - 6)}`
 }
+import qs from 'query-string'
+
+export function formUrlQuery({
+  params,
+  key,
+  value,
+}: {
+  params: string
+  key: string
+  value: string | null
+}) {
+  const currentUrl = qs.parse(params)
+
+  currentUrl[key] = value
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true }
+  )
+}
